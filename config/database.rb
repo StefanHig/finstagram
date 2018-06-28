fe92de1f61a86ddaffd32b36a4213d96c00f9cf4
Stefan Higgins <stef18@shaw.ca>
2018-06-28 23:06:42 +0000

    Readies app for deployment

--- config/database.rb
+++ config/database.rb
@@ -6,7 +6,7 @@ configure do
 
   set :database, {
     adapter: "sqlite3",
-    database: "db/db.sqlite3"
+    database: "postgres://bntxhwrtrzpaem:43f9ea63086381a69d6ba89b4b46c3b38f5fcf49c47a9a20b6d43b2adcd3b1a1@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d5uvgs54v2rte6"
   }
 
   # Load all models from app/models, using autoload instead of require
